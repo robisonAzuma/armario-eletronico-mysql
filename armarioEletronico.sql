be02cabe8297a77a3336866ada4e219a0d5f0999
Robison Azuma <58313260+robisonAzuma@users.noreply.github.com>
2020-04-14 01:09:43 -0300

    Update armarioEletronico.sql

--- armarioEletronico.sql
+++ armarioEletronico.sql
@@ -116,3 +116,14 @@ chave int not null,
 constraint fk_nivelPermissao FOREIGN KEY (nivel) REFERENCES nivelusuario(id),
 constraint fk_chavePermissao FOREIGN KEY (chave) REFERENCES chave(id)
 ) ENGINE = InnoDB;
+
+delimiter $$
+CREATE DEFINER=`root`@`localhost` TRIGGER `retirarChave_AFTER_UPDATE` AFTER UPDATE ON `retirarChave ` FOR EACH ROW BEGIN
+if new.entregue = 0 then
+insert into retirarChave  values (null,null,null,null,now(),null,0);
+if new.entregue = 1 then
+insert into retirarChave  values (null,null,null,null,,null,now(),1);
+END if;
+end if;
+end
+delimiter ;
